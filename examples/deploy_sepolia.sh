8683ea5732eee07798b7c4233750055f811ff13f
yanwenzhe519-ctrl <yanwenzhe519@example.com>
2026-06-11 23:06:03 +0800

    fix example scripts

--- examples/deploy_sepolia.sh
+++ examples/deploy_sepolia.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-# 部署到 Sepolia 测试网
-cd /c/Users/lenovo/Desktop/IP-Weave
-PYTHONPATH=/c/Users/lenovo/Desktop/IP-Weave python deploy.py YOUR_PRIVATE_KEY
+export PYTHONPATH=$(pwd)
+python deploy.py $1
--- examples/run_pepe.sh
+++ examples/run_pepe.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-# 运行 Pepe IP 衍生内容生成
-cd /c/Users/lenovo/Desktop/IP-Weave
-PYTHONPATH=/c/Users/lenovo/Desktop/IP-Weave python src/run_agent.py --ip pepe
+export PYTHONPATH=$(pwd)
+python src/run_agent.py --ip pepe
