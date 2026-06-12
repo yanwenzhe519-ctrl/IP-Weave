a7865337f693238a44cdcddd5e3cbbf238b9d2ed
yanwenzhe519-ctrl <yanwenzhe519@gmail.com>
2026-06-12 09:23:00 +0800

    update skills init

--- src/skills/__init__.py
+++ src/skills/__init__.py
@@ -1,3 +1,3 @@
 from .ip_reader import read_ip_data
-from .content_creator import create_story, create_script, create_assets
+from .content_creator import analyze_style, create_story, create_script, create_assets
 from .nft_publisher import deploy_nft
