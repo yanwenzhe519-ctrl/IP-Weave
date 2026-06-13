c45db1dab233cad98157b4a461b17a5d2862b95b
yanwenzhe519-ctrl <yanwenzhe519@gmail.com>
2026-06-13 11:09:57 +0800

    cleanup: remove src/skills/script_writer.py

--- src/skills/script_writer.py
+++
@@ -1,6 +0,0 @@
-
-from src.content.generators import ScriptGenerator
-gen = ScriptGenerator()
-
-def write_script(story="", style_profile=None):
-    return {"script": gen.generate(story, style_profile)}
